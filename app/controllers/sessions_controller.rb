875ceb5bd191606de498dd4c2305390116c808b1
ewycheung <cheung_eddie@rogers.com>
2021-04-06 22:43:57 -0400

    Add message create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,7 @@
 class SessionsController < ApplicationController
-  def new    
+  before_action :logged_in_redirect, only: [:new, :create]
+  
+  def new  
   end
 
   def create    
@@ -19,4 +21,13 @@ class SessionsController < ApplicationController
     flash[:success] = "Logged out"
     redirect_to login_path
   end
+
+  private
+
+    def logged_in_redirect
+      if logged_in?
+        flash[:error] = "You are already logged in"
+        redirect_to root_path
+      end
+    end
 end
